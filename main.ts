b0603609bcee69e5a3b6003a7ccf144826d7513e
234-star <132486789+234-star@users.noreply.github.com>
2023-05-03 18:20:35 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,96 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 3 1 3 . . . . . . 
+        . . . . . . 2 3 1 3 2 . . . . . 
+        . . . . . . 2 1 1 1 2 . . . . . 
+        . . . . . . 2 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 2 3 1 3 2 . . . . . 
+        . . . . . . . 2 2 2 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, -50)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    sprites.destroy(sprite, effects.disintegrate, 500)
+    info.changeScoreBy(1)
+    music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.InBackground)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+    scene.centerCameraAt(0, 0)
+    info.changeLifeBy(-1)
+})
+let fire: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect(500)
+scene.setBackgroundColor(8)
+mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 7 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 7 . . . . . . . 
+    . . . . . . 8 8 5 6 . . . . . . 
+    . . . . . . 8 7 5 6 . . . . . . 
+    . . . . . c c c 6 6 6 . . . . . 
+    . . . . 8 8 7 7 7 5 6 6 . . . . 
+    . . 8 f f f c c 6 6 f f 6 6 . . 
+    . 8 8 8 8 6 6 7 7 7 7 5 7 6 6 . 
+    8 8 8 8 8 8 6 6 7 7 7 5 7 7 6 6 
+    8 8 8 8 8 8 6 6 7 7 7 7 5 7 6 6 
+    `, SpriteKind.Player)
+mySprite.setPosition(80, 95)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(1000, function () {
+    fire = sprites.createProjectileFromSide(img`
+        ..........bbbbbb................
+        .......bbb444444bb..............
+        .....2244444ddd444b.............
+        ....244444444dddd44e............
+        ...244444444444ddd4be...........
+        ..244444444444444d44be..........
+        .2b444444444444444d4be..........
+        .2b44444444444444444bbe.........
+        2bbb4444444444444444bbe.........
+        2bbb4444444444444444bbe.........
+        2bb4b4444444444444444bbe........
+        2bb4444444444444444444be........
+        2bb44444444444444444444e........
+        2bbb444bbb4444444444444e........
+        22bbb444bb4bb444444444be........
+        .2bbbbb44bbbb44444444bbe........
+        .22bbbbbbbb44bbb444444bbe.......
+        ..eeebbbbbbb44bbb444444be.......
+        ...eeeeebbbbbbbb44b4444be.......
+        .....eeeeee222bb44bbb4bbe.......
+        .......eeeee222bb44bbbbee.......
+        ............e222bbbbbbbec.......
+        ..............ee2bbbbeebdb......
+        .................eeeeecdddb.....
+        .......................cd11bbbb.
+        ........................cd111dbb
+        .........................b11111c
+        .........................c11dd1c
+        .........................cd1dbc.
+        .........................cb11c..
+        ..........................ccc...
+        ................................
+        `, 0, 50)
+    fire.x = randint(0, scene.screenWidth())
+    fire.setKind(SpriteKind.Enemy)
+})
